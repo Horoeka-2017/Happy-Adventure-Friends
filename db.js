eaf0448758278c3fc594590f230302e4a02845d5
Nathan Lamb <nathanlamb022@gmail.com>
2017-07-13 19:51:55 +1200

    Comment out empty objets and variables

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-function getData (cb) {
-  fs.readFile('./data.json', (err, puppyData) => {
-    if (err) {
-      cb(err)
-      return
-    }
-    const objPuppy = JSON.parse(puppyData)
-    cb(null, objPuppy)
-  })
-}
+// function getData(cb) {
+//   fs.readFile('./data.json', (err, puppyData) => {
+//     if (err) {
+//       cb(err)
+//       return
+//     }
+//     const objPuppy = JSON.parse(puppyData)
+//     cb(null, objPuppy)
+//   })
+// }
 
-module.exports = {
-  getData: getData,
-  saveData: saveData
-}
+// module.exports = {
+//   getData: getData,
+//   saveData: saveData
+// }
