6dd5fc1659581db182ec14408fdd810def3ebcbe
Joshua Fogg <joshuafoggnz@gmail.com>
2017-07-13 17:40:48 +1200

    updated index and server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var hbs = require('express-handlebars')
+var bodyParser = require('body-parser')
+const routes = require('./routes')
+var app = express()
+const fs = require('fs')
+
+// Middleware
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/', routes)
+
+module.exports = app
