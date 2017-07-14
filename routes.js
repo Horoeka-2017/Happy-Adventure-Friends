820c91a94d432c6b795b2c71fbf33dc32c4d8668
Nathan Lamb <nathanlamb022@gmail.com>
2017-07-14 15:17:48 +1200

    Got story text to display

--- routes.js
+++ routes.js
@@ -2,32 +2,35 @@ var express = require('express')
 var router = express.Router()
 var db = require('./db.js')
 var path = require('path')
+var fs = require('fs')
 
-// var storyStageOne = require('./data-files/story-stage1')
-// router.get('/', (req, res) => {
+var storyStageOne = require('./data-files/story-stage1.json')
+  // router.get('/', (req, res) => {
 
 //   res.sendFile(path.join(__dirname + '/index.html'))
 // })
 
-router.get('url/1', (req, res) => {
-  fs.readFile('/data-files/story-stage1', 'utf8', (err, storyComplete) => {
+router.get('/url/:id', (req, res) => {
+  fs.readFile('./data-files/story-stage1.json', 'utf8', (err, storyComplete) => {
     if (err) {
-      return res.status(500).send('An error has occurred')
+      return res.status(500).send(err.message)
     } else {
       var storyObj = JSON.parse(storyComplete)
 
-      var foundStory = storyObj.stories.find((storySeg) => {
-        if (storySeg.id === Number(req.params.id)) {
+
+      var foundStory = storyObj.stories.find((story) => {
+        if (story.id === Number(req.params.id)) {
+          console.log(story.id)
           return true
-            // this can be changed to return storySeg.id === Number(req.params.id)
+
         }
       })
       res.render('play-page', foundStory)
+
+
     }
   })
 })
-
-stories.options[0]
 
 
 
