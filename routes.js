264c1b0d19e1b91d49cb569e2c2baeda542b823c
Nathan Lamb <nathanlamb022@gmail.com>
2017-07-14 13:32:23 +1200

    Change to array

--- routes.js
+++ routes.js
@@ -3,15 +3,33 @@ var router = express.Router()
 var db = require('./db.js')
 var path = require('path')
 
+var storyStageOne = require('./datafiles/story-stage1.json')
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-router.get('/story/:id', (req, res) => {
-      if (req.params.id === //storyid1)
-      })
-    // router.get('/character', (req, res) => {
-    //   res.render('./layouts/character', characterChoice)
-    // })
+router.get('url', (req, res) => {
+  fs.readFile('/datafiles/story-stage1.json', 'utf8', (err, storyStage1) => {
+    if (err) {
+      return res.status(500).send('An error has occurred')
+    } else {
+      var storyObj = JSON.parse(storyStage1)
 
-    module.exports = router
+      var foundStory = storyObj.stories.find(storySeg) => {
+        if (storySeg.id === Number(req.params.id)) {
+          return true
+        }
+      }
+      res.render('play-page', foundStory)
+    }
+  })
+})
+
+
+
+
+// router.get('/character', (req, res) => {
+//   res.render('./layouts/character', characterChoice)
+// })
+
+module.exports = router
